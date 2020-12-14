d3c98fa2eda71d696ab3266a65e69f5a57f3db2c
Justin Mello <mello23@llnl.gov>
2020-12-13 21:07:38 -0800

    Bringing in Raul's changes.

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
+
 const express = require("express");
 const app = express();
+const pool = require("./dbPools.js");
+const fetch = require("node-fetch");
+
+
 app.engine('html', require('ejs').renderFile);
 app.use(express.static("public"));
-const fetch = require("node-fetch");
 
 //Routing
 app.get("/", function(req, res){
-	res.render("index.html");
+	res.render("index.ejs");
 });
 
 //Score Page
-app.get("/scores", async function(req, res){
-	
-	let url = `https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard`;
-    
-    
+app.get("/schedules", async function(req, res){
+
+    let url = `https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard`;
+
+
     let response = await fetch(url);
-    
+
     let data = await response.json();
-          
+
     console.log(data);
-    
+
     let scheduleArray = [];
     let nameArray = [];
     let dateArray = [];
@@ -29,34 +33,88 @@ app.get("/scores", async function(req, res){
     let awayTeamLogo = [];
     let homeTeamRecord = []
     let awayTeamRecord = [];
-    
+
     let currentWeek = data.week.number;
-    
-    for( let i = 0; i < 13; i++) {
-    	scheduleArray.push(data.events[i].shortName)
-    	nameArray.push(data.events[i].name);
-     	dateArray.push(data.events[i].date);
-     	venueArray.push(data.events[i].competitions[0].venue.fullName);
+
+    for( let i = 0; i < 16; i++) {
+        scheduleArray.push(data.events[i].shortName)
+        nameArray.push(data.events[i].name);
+         dateArray.push(data.events[i].date);
+         venueArray.push(data.events[i].competitions[0].venue.fullName);
         homeTeamLogo.push(data.events[i].competitions[0].competitors[0].team.logo);
         awayTeamLogo.push(data.events[i].competitions[0].competitors[1].team.logo);
         homeTeamRecord.push(data.events[i].competitions[0].competitors[0].records[0].summary);
         awayTeamRecord.push(data.events[i].competitions[0].competitors[1].records[0].summary);
     }
 
-	
-	res.render("schedules.html",{"currentWeek":currentWeek,"scheduleArray":scheduleArray, 
-	"nameArray":nameArray, "dateArray":dateArray, "venueArray":venueArray, "homeTeamLogo":homeTeamLogo, 
-	"awayTeamLogo":awayTeamLogo, "awayTeamRecord":awayTeamRecord, "homeTeamRecord":homeTeamRecord});
+
+    res.render("schedules.ejs",{"currentWeek":currentWeek,"scheduleArray":scheduleArray, 
+    "nameArray":nameArray, "dateArray":dateArray, "venueArray":venueArray, "homeTeamLogo":homeTeamLogo, 
+    "awayTeamLogo":awayTeamLogo, "awayTeamRecord":awayTeamRecord, "homeTeamRecord":homeTeamRecord});
 });
 
 //Ticket Page
 app.get("/tickets", function(req, res){
-	res.render("tickets.html");
+	res.render("tickets.ejs");
 });
 
+
+app.get("/store", function (req, res)
+{
+    res.render("store.ejs");
+
+
+});
+
+app.get("/api/updateTicketDatabase", function (req, res)
+{
+    let sql;
+
+    let sqlParams;
+    
+    switch (req.query.action) {
+    case "add": sql = "INSERT INTO ticket (price, teams) VALUES (?,?)";
+                sqlParams = [req.query.price, req.query.teams];
+                break;
+    }
+
+  
+pool.query(sql, sqlParams, function (err, rows, fields) {
+    if (err) throw err;
+    console.log(rows);
+    res.send(rows.affectedRows.toString());
+  });
+
+});
+
+app.get("/api/updateDatabase", function (req, res)
+{
+    let sql;
+
+    let sqlParams;
+    
+    switch (req.query.action) {
+    case "add": sql = "INSERT INTO merchandise (total, value) VALUES (?,?)";
+                sqlParams = [req.query.total, req.query.value];
+                break;
+    }
+
+  
+pool.query(sql, sqlParams, function (err, rows, fields) {
+    if (err) throw err;
+    console.log(rows);
+    res.send(rows.affectedRows.toString());
+  });
+
+});
+
+
 //Store
-app.get("/store", function(req, res){
-	res.render("store.html");
+app.get("/store-results", function(req, res){
+  
+    res.render("store-results.ejs");
+    
+    
 });
 
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,63 @@
 /* global newPrice $*/
-$(document).ready(async function()
+
+var newPrice=0;
+var basePrice=0;
+var sum =0;
+    
+ $(".calculate").change(function(res, req) 
 {
 
-});
-    var newPrice=0;
-    var basePrice=0;
-    
-    $(".calculate").change(function() {
     newPrice = basePrice;
     $(".calculate option:selected").each(function() {
         newPrice += $(this).data('price')
+        
+        document.getElementById("myButton").disabled = false;
     });
-    
+
     $("#total").html(newPrice.toFixed(2));
 });
+
+
+$("#myButton").on("click", async function()
+{
+    window.location.href='/store-results';
+    let queryString = window.location.search;
+    let urlParams   = new URLSearchParams(queryString);
+    // let check =  $(this).html().trim(); 
+    //  $("#check").val(check);
+    let total="";
+    let value = 0;
+    value= $("#check").val();  
+    
+    
+    updateDatabase("add", total, value);
+    
+});
+
+async function updateDatabase(action,total,value)
+{
+       let url = `/api/updateDatabase?action=${action}&total=${newPrice}&value=${value}`
+    await fetch(url);
+}
+
+
+function calculate(){
+    
+    var sum = parseIntX($("#setId").val()) + parseIntX($("#setId1").val()) + parseIntX($("#setId2").val()) + parseIntX($("#setId3").val());
+        
+      $('#check').val(sum);
+      
+}
+
+function parseIntX(num){
+    return (isNaN(parseInt(num))) ? 0 : parseInt(num);
+}
+
+// gets the sum of all four option inputs. 
+$("#setId, #setId1, #setId2, #setId3").change(function() {
+    
+    calculate();
+});
+
+
 
